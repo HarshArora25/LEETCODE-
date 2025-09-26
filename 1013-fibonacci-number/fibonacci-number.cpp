add4b8af41804703b4bd12b4e27d96d464eb1c82
HarshArora25 <harsharora.2207@gmail.com>
2025-09-26 18:49:14 +0530

    Time: 2462 ms (22.33%) | Memory: 364.7 MB (11.07%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+   int  find(vector<int>&dp,int n){
+          if(n<=1) return n;
+      if(dp[n] !=-1) return dp[n];
+       return dp[n]= fib(n-1) + fib(n-2); 
+     }
+    int fib(int n) {
+    vector<int>dp(n+1,-1);
+    return find(dp,n);
+    }
+};
