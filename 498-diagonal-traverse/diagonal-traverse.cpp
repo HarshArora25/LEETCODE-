36326582686964d6a8349a78d4511a83a2948344
HarshArora25 <harsharora.2207@gmail.com>
2025-10-21 12:43:10 +0530

    Time: 23 ms (22.63%) | Memory: 30.3 MB (16.73%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        unordered_map<int,vector<int>>mpp;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+            mpp[i+j].push_back(mat[i][j]);
+            }
+        }
+        vector<int>x;
+        for(int k=0;k<mat.size()+mat[0].size();k++){
+            if(k%2==0)
+            reverse(mpp[k].begin(),mpp[k].end());
+            for(int val:mpp[k])
+            x.push_back(val);
+        }
+        return x;
+    }
+};
