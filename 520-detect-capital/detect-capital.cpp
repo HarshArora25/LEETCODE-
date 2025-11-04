c604af95a102330c6d26a009abf251df6c23b1e7
HarshArora25 <harsharora.2207@gmail.com>
2025-11-04 20:21:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (6.17%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    private:
+    bool allcaptial(string word){
+        for(char&ch:word){
+         if(ch<'A' || ch>'Z')
+         return false;
+        }
+        return true;
+    }
+
+    bool allsmall(string word){
+     for(char&ch:word){
+         if(ch<'a' || ch>'z')
+         return false;
+        }
+        return true;    
+    }
+public:
+    bool detectCapitalUse(string word) {
+     if( allcaptial(word) || allsmall(word) || allsmall(word.substr(1))){
+        return true;
+     }   
+     return false;
+    }
+};
