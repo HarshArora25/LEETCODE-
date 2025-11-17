e729ae7b78fde671a094dacf518dd3960c0456cb
HarshArora25 <harsharora.2207@gmail.com>
2025-11-18 00:10:36 +0530

    Time: 9 ms (24.57%) | Memory: 26.9 MB (99.52%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,30 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    int find(int x, const vector<int>& inorder, int start, int end) {
-        for (int i = start; i <= end; i++) {
-            if (x == inorder[i])
-                return i;
+    private:
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int start,int end,int &idx){
+        if(start>end) return NULL;
+        int rootvl=preorder[idx];
+        idx++;
+        int i=start;
+        for( ;i<=end;i++){
+            if(inorder[i]==rootvl)
+            break;
         }
-        return -1;
-    }
-
-    TreeNode* ressu(vector<int>& preorder, vector<int>& inorder, int start, int end, int &index) {
-        if (start > end) return nullptr;
-
-        int pos = find(preorder[index], inorder, start, end);
-
-        TreeNode* root = new TreeNode(preorder[index]);
-        index++; 
-
-        root->left = ressu(preorder, inorder, start, pos - 1, index);
-        root->right = ressu(preorder, inorder, pos + 1, end, index);
-
+        // idx++;
+        TreeNode* root=new TreeNode(rootvl);
+        root->left=solve(preorder,inorder,start,i-1,idx);
+        root->right=solve(preorder,inorder,i+1,end,idx);
         return root;
     }
-
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int index = 0;
-        return ressu(preorder, inorder, 0, preorder.size() - 1, index);
+       int n=preorder.size();
+       int idx=0;
+       return solve(preorder,inorder,0,n-1,idx); 
     }
 };
