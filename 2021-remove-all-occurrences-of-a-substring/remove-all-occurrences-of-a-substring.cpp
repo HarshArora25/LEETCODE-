7f2d20dab47056b5aff8c0b529b5ed7f51468f8a
HarshArora25 <harsharora.2207@gmail.com>
2025-11-17 17:27:18 +0530

    Time: 1 ms (51.32%) | Memory: 9.3 MB (68.75%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(true){
+            int idx=s.find(part);
+            if(idx==string::npos) break;
+            s.erase(idx,part.size());
+        }
+        return s;
+    }
+};
