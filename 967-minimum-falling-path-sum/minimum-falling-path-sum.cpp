089f7ad3e32f4404e4fe2d3f880e79dc5d797910
HarshArora25 <harsharora.2207@gmail.com>
2025-11-30 16:17:56 +0530

    Time: 1 ms (44.38%) | Memory: 14.5 MB (81.84%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,26 +1,60 @@
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int m=matrix.size();
+//         // int l=1e9;
+//        vector<int>hii(m),byy(m);
+//         for(int col=0;col<m;col++)
+//        hii[col]=matrix[m-1][col];
+//         for(int i=m-2;i>=0;i--){
+//         for(int j=0;j<m;j++){
+//             int ab=1e9,cd=1e9,ef=1e9;
+//             if(j-1>=0)
+//              ab=matrix[i][j] +  hii[j-1];
+//              cd=matrix[i][j] +  hii[j];
+//             if(j+1<m)
+//              ef=matrix[i][j] + hii[j+1];
+//             byy[j]=min({ab,cd,ef});
+//             }
+//             hii=byy;
+//         }
+//         int x=1e9;
+//         for(int i=0;i<m;i++){
+//          x=min(x,byy[i]);
+//         }
+//         return x;
+//     }
+// };
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int l=1e9;
-        vector<vector<int>>dp(m,vector<int>(m,-1));
-        for(int col=0;col<m;col++)
-        dp[m-1][col]=matrix[m-1][col];
-        for(int i=m-2;i>=0;i--){
-           int ab=1e9,cd=1e9,ef=1e9;
-        for(int j=0;j<m;j++){
-            if(j-1>=0)
-             ab=matrix[i][j] +  dp[i+1][j-1];
-             cd=matrix[i][j] +  dp[i+1][j];
-            if(j+1<m)
-             ef=matrix[i][j] + dp[i+1][j+1];
-            dp[i][j]=min(min(ab,cd),ef);
+        int n = matrix.size();
+        vector<int> prev(n), curr(n);
+        // Last row → prev
+        for (int j = 0; j < n; j++) 
+            prev[j] = matrix[n-1][j];
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                int leftDiag = 1e9, down = 1e9, rightDiag = 1e9;
+
+                if (j - 1 >= 0) 
+                    leftDiag = matrix[i][j] + prev[j-1];
+
+                down = matrix[i][j] + prev[j];
+
+                if (j + 1 < n) 
+                    rightDiag = matrix[i][j] + prev[j+1];
+
+                curr[j] = min({leftDiag, down, rightDiag});
             }
+
+            prev = curr;
         }
-        int x;
-        for(int i=0;i<m;i++){
-         x=min(x,dp[0][i]);
-        }
-        return x;
+
+        int ans = 1e9;
+        for (int j = 0; j < n; j++)
+            ans = min(ans, prev[j]);
+
+        return ans;
     }
 };
