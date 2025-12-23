1b3154c71f9de896300bedd4f1f17240613b7d24
HarshArora25 <harsharora.2207@gmail.com>
2025-12-23 18:04:32 +0530

    Time: 68 ms (6.05%) | Memory: 31.7 MB (14.70%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,32 +1,20 @@
 class Solution {
-    int helper(string &s, string &a, int i, int j,
-               vector<vector<int>> &dp) {
-
-        if (i < 0 || j < 0)
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s[i] == a[j]) {
-            return dp[i][j] = 1 + helper(s, a, i - 1, j - 1, dp);
-        } else {
-            return dp[i][j] = max(
-                helper(s, a, i - 1, j, dp),
-                helper(s, a, i, j - 1, dp)
-            );
-        }
+    int helper(string &s,string &a,int idx1,int idx2,vector<vector<int>>&dp){
+        if(idx1<0 || idx2<0 ) return 0;
+        if(dp[idx1][idx2] !=-1) return dp[idx1][idx2];
+        if(s[idx1]==a[idx2])
+        return dp[idx1][idx2]= 1+helper(s,a,idx1-1,idx2-1,dp);
+        else
+        return dp[idx1][idx2]=max(helper(s,a,idx1-1,idx2,dp) , helper(s,a,idx1,idx2-1,dp));
+        return 0;
     }
-
 public:
     int minInsertions(string s) {
-        string a = s;
-        reverse(a.begin(), a.end());
-
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        int lps = helper(s, a, n - 1, n - 1, dp);
-        return n - lps;
+       string a=s;
+       reverse(a.begin(),a.end());
+       int idx=s.size();
+       vector<vector<int>>dp(idx+1,vector<int>(idx+1,-1));
+      int tot= helper(s,a,idx-1,idx-1,dp); 
+      return idx-tot;
     }
 };
