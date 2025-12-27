75b5a6f73082190bcf379648e8a2b5bde04ef475
HarshArora25 <harsharora.2207@gmail.com>
2025-12-28 00:35:00 +0530

    Time: 3 ms (91.88%) | Memory: 13.1 MB (73.32%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int helper(string &word1,string &word2,int n,int m, vector<vector<int>>&dp){
-        if(n<0) return m+1;
-        if(m<0) return n+1;
-        if(dp[n][m] !=-1) return dp[n][m];
-        if(word1[n]==word2[m]){
-           return dp[n][m]=helper(word1,word2,n-1,m-1,dp);
-        }
-        else
-            return dp[n][m]=1+min(helper(word1,word2,n-1,m,dp),min(helper(word1,word2,n,m-1,dp),helper(word1,word2,n-1,m-1,dp)));
-        return 0;
-    }
+    // int helper(string &word1,string &word2,int n,int m, vector<vector<int>>&dp){
+    //     if(n<0) return m+1;
+    //     if(m<0) return n+1;
+    //     if(dp[n][m] !=-1) return dp[n][m];
+    //     if(word1[n]==word2[m]){
+    //        return dp[n][m]=helper(word1,word2,n-1,m-1,dp);
+    //     }
+    //     else
+    //         return dp[n][m]=1+min(helper(word1,word2,n-1,m,dp),min(helper(word1,word2,n,m-1,dp),helper(word1,word2,n-1,m-1,dp)));
+    //     return 0;
+    // }
     int minDistance(string word1, string word2) {
      int n=word1.size();
      int m=word2.size();
-     vector<vector<int>>dp(n,vector<int>(m+1,-1));
-    return helper(word1,word2,n-1,m-1,dp);   
+     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    // return helper(word1,word2,n-1,m-1,dp);   
+     for(int i=0;i<=n;i++) dp[i][0]=i;
+     for(int j=0;j<=m;j++)  dp[0][j]=j;
+
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(word1[i-1]==word2[j-1])
+          dp[i][j]=dp[i-1][j-1];
+        else
+             dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1])); 
+        }
+     }
+     return dp[n][m];
     }
 };
