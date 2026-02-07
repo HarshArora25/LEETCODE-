a574aaf8592ab87c1dc39bb2287ee259aa6e8af1
HarshArora25 <harsharora.2207@gmail.com>
2026-02-07 23:50:08 +0530

    Time: 25 ms (38.13%) | Memory: 43.6 MB (59.88%) - LeetSync

--- 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
@@ -1,25 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    string helper(TreeNode* root, unordered_map<string, int>& mpp, vector<TreeNode*>& res) {
-        if (!root) return "N";
-
-        string s = to_string(root->val) + "," +
-                   helper(root->left, mpp, res) + "," +
-                   helper(root->right, mpp, res);
-
-        if (mpp[s] == 1) {
-            res.push_back(root);
-        }
-
+public:
+    string help(TreeNode*root,unordered_map<string,int>&mpp,vector<TreeNode*>&res){
+        if(root==NULL)
+        return "N";
+        string s = to_string(root->val)+ "," + help(root->left,mpp,res) + "," + help(root->right,mpp,res);
+        if(mpp[s]==1)
+        res.push_back(root);
         mpp[s]++;
         return s;
     }
-
-public:
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        unordered_map<string, int> mpp;
-        vector<TreeNode*> res;
-        helper(root, mpp, res);
-        return res;
+    unordered_map<string,int>mpp;
+    vector<TreeNode*>res;
+    help(root,mpp,res);
+    return res;
     }
 };
