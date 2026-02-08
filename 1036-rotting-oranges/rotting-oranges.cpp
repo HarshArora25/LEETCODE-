b7d6f5d75d7b3508680576500d0f5e32a8eba544
HarshArora25 <harsharora.2207@gmail.com>
2026-02-08 20:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (43.95%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,101 +1,37 @@
-// class Solution {
-// public:
-//     int orangesRotting(vector<vector<int>>& grid) {
-//         int n=grid.size();
-//         int m=grid[0].size();
-//         int vis[n][m];
-//       queue<pair<pair<int,int>,int>>qu;
-//       for(int i=0;i<n;i++){
-//         for(int j=0;j<m;j++){
-//             if(grid[i][j]==2){
-//             qu.push({{i,j},0});
-//             vis[i][j]=2;
-//             }
-//             else
-//             vis[i][j]=0;
-//         }
-//       }
-//       vector<int>delrow={-1,0,1,0};
-//         vector<int>delcol={0,1,0,-1};
-//       while(!qu.empty()){
-//         int nrow=qu.front().first.first;
-//         int ncol=qu.front().first.second;
-//         int t=q.front().second;
-//         qu.pop();
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<m;j++){
-//               int nw=nrow + delrow[i];
-//               int nc=ncol + delcol[j];
-//               if(nw>=0 && nw<n-1 && nc>=0 && nc<m && grid[i][j]=1 && vis[nw][nr] !=2){
-//                qu.push({{nw,nc},t+1});
-//                vis [n][m]=2;
-//               }
-//             }
-//         }
-//     //   }
-//          for(int i=0;i<n;i++){
-//             for(int j=0;j<m;j++){
-//                 if(vis[i][j]!=2 || grid[i][j]==1 ) return -1;
-//             }
-//          }
-
-//       return t;
-//     }
-// };
 class Solution {
+    int step;
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<pair<int,int>, int>> qu;
-
-        int fresh = 0;
-        int time = 0;
-
-        // initial setup
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    qu.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
+      queue<pair<pair<int,int>,int>>qu;
+      vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
+      int nr[]={-1,0,1,0};
+      int nc[]={0,1,0,-1};
+      int fresh=0;
+      for(int i=0;i<grid.size();i++){
+        for(int j=0;j<grid[0].size();j++){
+           if(grid[i][j]==2){
+            qu.push({{i,j},0});
+            vis[i][j]=1;
+           }
+           if(grid[i][j]==1)
+           fresh++;
         }
-
-        int delrow[4] = {-1, 0, 1, 0};
-        int delcol[4] = {0, 1, 0, -1};
-
-        // BFS
-        while (!qu.empty()) {
-            int row = qu.front().first.first;
-            int col = qu.front().first.second;
-            int t   = qu.front().second;
-            qu.pop();
-
-            time = max(time, t);
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-
-                if (nrow >= 0 && nrow < n &&
-                    ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == 1 &&
-                    vis[nrow][ncol] != 2) {
-
-                    qu.push({{nrow, ncol}, t + 1});
-                    vis[nrow][ncol] = 2;
-                    grid[nrow][ncol] = 2;
-                    fresh--;
-                }
-            }
+      }  
+      while(!qu.empty()){
+       int x=qu.front().first.first;
+       int y=qu.front().first.second;
+      step=qu.front().second;
+       qu.pop();
+       for(int i=0;i<4;i++){
+        int newr=nr[i]+x;
+        int newc=nc[i]+y;
+        if(newr>=0 && newr<grid.size() && newc>=0 && newc<grid[0].size() && !vis[newr][newc] && grid[newr][newc]==1){
+            qu.push({{newr,newc},step+1});
+            vis[newr][newc]=1;
+            fresh--;
         }
-
-        if (fresh > 0) return -1;
-        return time;
+       }
+      }
+      return (fresh>0)?-1:step;
     }
 };
