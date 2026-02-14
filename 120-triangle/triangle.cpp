934f88a27060703ec09efd1f8cebc59c6feea613
HarshArora25 <harsharora.2207@gmail.com>
2026-02-14 11:20:20 +0530

    Time: 1 ms (19.53%) | Memory: 13.3 MB (42.58%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-        //    int mini=INT_MAX;
-        void findkro(vector<vector<int>>& triangle,int n,int i,int j,vector<vector<int>>&dp){
-          for(int j=0;j<n;j++)
-          dp[n-1][j]=triangle[n-1][j];
-          for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int d=triangle[i][j] + dp[i+1][j];
-                int l=triangle[i][j] + dp[i+1][j+1];
-                dp[i][j]=min(d,l);
-             }
-          }
-        }
     int minimumTotal(vector<vector<int>>& triangle) {
-        // int sum=0;
-        int m=triangle.size();
-        int n=triangle[0].size();
-        vector<vector<int>>dp(m,vector<int>(m,-1));
-         findkro(triangle,m,0,0,dp);
-         return dp[0][0];
-      
+       vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),-1));
+       for(int j=0;j<triangle.size();j++){
+        dp[triangle.size()-1][j]=triangle[triangle.size()-1][j];
+       }
+
+       for(int i=triangle.size()-2;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+        }
+       }
+       return dp[0][0];
     }
 };
