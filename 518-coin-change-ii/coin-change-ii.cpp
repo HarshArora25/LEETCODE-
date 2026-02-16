e89ad42f49a0f646d8136ff2d58d8504eb3ea760
HarshArora25 <harsharora.2207@gmail.com>
2026-02-17 01:19:05 +0530

    Time: 44 ms (23.85%) | Memory: 36.9 MB (34.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
-    private:
-    int helper(int amount,vector<int>&coins,int index,vector<vector<int>>&dp){
-         if(amount==0) return 1;
-        if(index<0) return 0;
-        if(dp[index][amount] !=-1 ) return dp[index][amount];
-        if(coins[index]>amount)
-        return dp[index][amount]=helper(amount,coins,index-1,dp);
-        else
-        return dp[index][amount]=helper(amount-coins[index],coins,index,dp)  + helper(amount,coins,index-1,dp);
+private:
+    int help(vector<int>& coins,int idx, vector<vector<int>>&dp,int amount){
+        if(amount==0) return 1;
+        if(idx==coins.size()) return 0;
+        if(dp[idx][amount] !=-1) return dp[idx][amount] ;
+        int nottake=help(coins,idx+1,dp,amount);
+        int take=0;
+        if(coins[idx]<=amount)
+        take=help(coins,idx,dp,amount-coins[idx]);
+        return dp[idx][amount]=take+nottake;
     }
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-      return  helper(amount,coins,n-1,dp);
+      vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+      return help(coins,0,dp,amount);  
     }
 };
