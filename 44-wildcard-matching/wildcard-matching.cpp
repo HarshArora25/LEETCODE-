ab010261e4689d7aa3af53381b7d83482ac1d13f
HarshArora25 <harsharora.2207@gmail.com>
2026-02-18 15:37:21 +0530

    Time: 149 ms (7.98%) | Memory: 54.8 MB (9.39%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,28 +1,29 @@
 class Solution {
-public:
-    bool helper(string &s,string &p,int n,int m, vector<vector<int>>&dp){
-        if(m<0 && n<0) return true;
-        if(m<0) return false;
-        if(n<0){
-            for(int i=0;i<=m;i++){
-                if(p[i] !='*')
-                return false;
-            }
-            return true;
+private:
+    bool helpme(string& s, string& p,int n,int m,int i,int j, vector<vector<int>>&dp){
+       if(i==n && j==m) return true;
+       if(j==m) return false;
+       if(i==n){
+        for(int k=j;k<m;k++){
+            if(p[k] !='*' ) return false;
         }
-              bool ans;
-        if(dp[n][m] !=-1) return dp[n][m];
-        if(s[n]==p[m] || p[m]=='?') ans= helper(s,p,n-1,m-1,dp);
-        else if (p[m] == '*')
-        ans = helper(s,p,n,m-1,dp) || helper(s,p,n-1,m,dp);
-        else
-       ans= false;
-        return dp[n][m] = ans;;
+        return true;
+       }
+        if(dp[i][j] !=-1) return dp[i][j];
+        if(s[i]==p[j] || p[j]=='?'){
+            return dp[i][j]=helpme(s,p,n,m,i+1,j+1,dp);
+        }
+        else if(p[j]=='*'){
+            return dp[i][j]=helpme(s,p,n,m,i,j+1,dp) || helpme(s,p,n,m,i+1,j,dp);
+        }
+        else 
+        return dp[i][j]= false;
     }
+public:
     bool isMatch(string s, string p) {
-       int n=s.size();
-       int m=p.size();
-       vector<vector<int>>dp(n,vector<int>(m+1,-1));
-     return  helper(s,p,n-1,m-1,dp); 
+    int n=s.size();
+    int m=p.size();
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    return helpme(s,p,n,m,0,0,dp);    
     }
 };
