5e7628e603ce094d85254b370b02817e6289cb0f
HarshArora25 <harsharora.2207@gmail.com>
2026-03-01 14:25:20 +0530

    Time: 981 ms (23.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,8 @@
+
+SELECT d.name as Department ,e.name as Employee,Salary from Employee e JOIN Department d
+ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
