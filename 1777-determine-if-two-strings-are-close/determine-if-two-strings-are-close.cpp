d5d6326da507794050859d661fa5d4b37986b444
HarshArora25 <harsharora.2207@gmail.com>
2026-03-12 17:26:08 +0530

    Time: 39 ms (19.41%) | Memory: 23.6 MB (53.86%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        vector<int>x(26,0);
-        vector<int>y(26,0);
-        
+        set<char>st1,st2;
+        vector<int>x(26,0),y(26,0);
+        if(word1.size() != word2.size())
+       return false;
         for(int i=0;i<word1.size();i++){
-            x[word1[i]-'a']++;
+          st1.insert(word1[i]);
+          st2.insert(word2[i]);
+           x[word1[i]-'a']++;
+           y[word2[i]-'a']++;
         }
-        for(int i=0;i<word2.size();i++){
-            y[word2[i]-'a']++;
-        }
-
-        for(int i=0;i<26;i++){
-            if( (x[i]==0 && y[i] !=0)  || (y[i]==0 && x[i]!=0 ) ){
-                return false;
-            }
-        }
-       sort(x.begin(),x.end());
-       sort(y.begin(),y.end());
-       return x==y;
-        
+        sort(x.begin(),x.end());
+        sort(y.begin(),y.end());
+        if(st1 != st2 || x !=y) return false;
+        return true;
     }
 };
