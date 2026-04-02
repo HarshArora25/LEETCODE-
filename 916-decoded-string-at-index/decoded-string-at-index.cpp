96509eea57eea6610d8aff9f09aa89a7cfbf1485
HarshArora25 <harsharora.2207@gmail.com>
2026-04-02 12:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (100.00%) - LeetSync

--- 916-decoded-string-at-index/decoded-string-at-index.cpp
+++ 916-decoded-string-at-index/decoded-string-at-index.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     string decodeAtIndex(string s, int k) {
-      long long size=0;
-      for(int i=0;i<s.size();i++){
-        if(isdigit(s[i]))
-        size=size*(s[i]-'0');
-      else
-      size+=1;
-      }
-      int n=s.size();
+        int n=s.size();
+    long long len=0;
+    for(int i=0;i<s.size();i++){
+        if(isalpha(s[i])){
+            len=len+1;
+        }
+        else{
+            len=len*(s[i]-'0');
+        }
+    }
+     int j=s.size()-1;
+
       for(int i=n-1;i>=0;i--){
-       k=k%size;
-       if(k==0 && isalpha(s[i]))
-       return string(1,s[i]);
-       if(isalpha(s[i]))
-       size--;
-       else
-       size=size/(s[i]-'0');
+        k=k%len;
+        if(k==0 && isalpha(s[i])){
+            return string(1,s[i]);
+        }
+        if(isalpha(s[i])){
+            len=len-1;
+        }
+        else{
+          len=len/(s[i]-'0');
+        }
       }
       return "";
     }
