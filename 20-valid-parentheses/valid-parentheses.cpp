f7faa027e29698adb960a5d5562af2184669736d
HarshArora25 <harsharora.2207@gmail.com>
2026-04-10 17:08:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+            if(!st.empty() &&((st.top()=='(' && s[i]==')')  ||  (st.top()=='[' && s[i]==']')  || (st.top()=='{' && s[i]=='}')))
+            st.pop();
+            else
+                st.push(s[i]);
+        }
+
+            if(st.empty()) return true;
+            return false;
+    }
+};
