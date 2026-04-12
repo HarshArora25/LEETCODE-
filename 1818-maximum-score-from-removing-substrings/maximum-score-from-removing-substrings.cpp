f580981c8d04f0abc63aab346c124cc956f5d39e
HarshArora25 <harsharora.2207@gmail.com>
2026-04-12 23:41:04 +0530

    Time: 62 ms (26.98%) | Memory: 28.7 MB (43.11%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,90 +1,64 @@
-// class Solution {
-//     private:
-//     string removeSubstr(string a,string value){
-//         stack<char>st;
-//      for(char& c:a){
-//         if(!st.empty() && st.top()==value[0] && c==value[1])
-//         st.pop();
-//         else
-//         st.push(c);
-//      }
-//      string temp="";
-//      while(!st.empty()){
-//         temp+=st.top();
-//         st.pop();
-//      }
-//      reverse(temp.begin(),temp.end());
-//      return temp;
-//     }
-// public:
-// // galat hai bilkul hi
-//     int maximumGain(string s, int x, int y) {
-// //         stack<char>st;
-// //         int maxstream=x>y?x:y;
-// //         int minstream=x<y?y:x;
-//         int n=s.length();
-// //         int result=0;
-// //         int count=0;
-// //     for(int i=0;i<n;i++){
-// //      while(!st.empty() && st.top()-'a'+s[i]-'a'==maxstream){
-// //         st.pop();
-// //         continue;
-// //      }
-// //      st.push(s[i]);
-// //     }
-// //     result+=((s.size()-st.size()/2)*maxstream);
-// //     while(!st.empty()){
-// //         int c=st.top()-'a';
-// //         st.pop();
-// //         if(!st.empty() && st.top()-'a'+c==maxstream){
-// //             count++;
-// //         }
-// //     }
-// // result+=count*minstream;
-// // return result;
-
-//     string maxstr=x>y?"ab":"ba";
-//     string minstream=x>y?"ba":"ab";
-//     string neww=removeSubstr(s,maxstr);
-//     int l=neww.length();
-//     int newl=n-l;
-//     int score=(newl/2)*max(x,y);
-//     string new1=removeSubstr(neww,minstream);
-//     int l1=new1.length();
-//     int newl1=newl-l1;
-//     score+=(newl1/2)*min(x,y);
-  
-//     return score;
-//     }
-// };
 class Solution {
-private:
-    string removeSubstr(string a, string value){
-        stack<char> st;
-        for(char c : a){
-            if(!st.empty() && st.top()==value[0] && c==value[1])
-                st.pop();
-            else
-                st.push(c);
-        }
-        string temp = "";
-        while(!st.empty()){
-            temp.push_back(st.top());
-            st.pop();
-        }
-        reverse(temp.begin(), temp.end());
-        return temp;
-    }
-
 public:
     int maximumGain(string s, int x, int y) {
-        string maxstr = x > y ? "ab" : "ba";
-        string minstr = x > y ? "ba" : "ab";
-        string neww = removeSubstr(s, maxstr);
-        int removed1 = s.length() - neww.length();
-        string new1 = removeSubstr(neww, minstr);
-        int removed2 = neww.length() - new1.length();
-        int score = (removed1/2)*max(x,y) + (removed2/2)*min(x,y);
-        return score;
+     int score=0;
+     stack<char>st;
+     string res="";
+     if(x>y){
+     for(int i=0;i<s.size();i++){
+        if(!st.empty() && st.top()=='a' && s[i]=='b'){
+        st.pop();
+        score+=x;
+        }
+        else
+        st.push(s[i]);
+     }
+   
+    //  while(!st.empty()){
+    //     res=st.top()+res;
+    //     st.pop();
+    //  }
+    while(!st.empty()){
+    res += st.top();   // ✅ instead of st.top() + res
+    st.pop();
+}
+reverse(res.begin(), res.end()); // ✅ add this
+     for(int i=0;i<res.size();i++){
+        if(!st.empty() && st.top()=='b' && res[i]=='a'){
+            st.pop();
+            score+=y;
+        }
+        else
+        st.push(res[i]);
+     }
+     }  
+     else{
+     for(int i=0;i<s.size();i++){
+        if(!st.empty() && st.top()=='b' && s[i]=='a'){
+        st.pop();
+        score+=y;
+        }
+        else
+        st.push(s[i]);
+     }
+    //  while(!st.empty()){
+    //     res=st.top()+res;
+    //     st.pop();
+    //  }
+    while(!st.empty()){
+    res += st.top();   // ✅ instead of st.top() + res
+    st.pop();
+}
+reverse(res.begin(), res.end()); // ✅ add this
+     for(int i=0;i<res.size();i++){
+        if(!st.empty() && st.top()=='a' && res[i]=='b'){
+            st.pop();
+            score+=x;
+        }
+        else
+        st.push(res[i]);
+     }
+     }
+     return score;
     }
 };
