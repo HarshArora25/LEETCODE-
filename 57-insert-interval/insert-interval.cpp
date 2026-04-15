75b9deeb665929785b76e08a873550d6e7fac0af
HarshArora25 <harsharora.2207@gmail.com>
2026-04-15 16:01:23 +0530

    Time: 3 ms (30.40%) | Memory: 21.7 MB (46.35%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> resultant;
         intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
-        vector<int> x = intervals[0];
-        for (int i = 1; i < intervals.size(); i++) {
-            int prev_start = x[0];
-            int prev_end = x[1];
-            int curr_start = intervals[i][0];
-            int curr_end = intervals[i][1];
-            if (prev_end < curr_start) {
-                resultant.push_back(x);
-                x = intervals[i];
-            } else {
-                x[0] = min(prev_start, curr_start);
-                x[1] = max(prev_end, curr_end);
-            }
+        sort(intervals.begin(),intervals.end(),[](auto&a ,auto& b){
+         return a[0]<b[0];
         }
-        resultant.push_back(x);
-        return resultant;
+        );
+
+        vector<vector<int>>intervel;
+        intervel.push_back(intervals[0]);
+
+        for(int i=1;i<intervals.size();i++){
+        if(intervel.back()[1]<intervals[i][0]){
+         intervel.push_back(intervals[i]);
+         }
+         else{
+            intervel.back()[1]=max(intervel.back()[1],intervals[i][1]);
+         }
+         }
+         return intervel;
     }
 };
-// m kya krra hu isme jo new inetrval h wo bhi jo input diya h usme hi push krr rha hu or phr use sort krra hu or phir dekhra hu iterate krke ki konse 2 interval overlap krre h jo overlap krre h wo vector m push krra hu
