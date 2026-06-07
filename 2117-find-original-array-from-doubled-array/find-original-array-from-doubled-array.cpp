fd89e0acc81c7c0dd370a306a1561d3416cf7131
HarshArora25 <harsharora.2207@gmail.com>
2026-06-07 17:55:16 +0530

    Time: 131 ms (75.14%) | Memory: 145.8 MB (43.26%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-    if(changed.size() %2 !=0)
-    return {};
-    sort(changed.begin(),changed.end());
-    unordered_map<int,int>mpp;
-    for(int num:changed){
-        mpp[num]++;
-    }    
-    vector<int>x;
-    for(int num: changed){
-        if(mpp[num]==0) continue;
-        if(mpp[2*num]==0) return{};
-        x.push_back(num);
-        mpp[num]--;
-        mpp[2*num]--;
-    }
-    return x;
+       int n=changed.size();
+       if(n%2 !=0) return {};
+       sort(changed.begin(),changed.end());
+       vector<int>original;
+       unordered_map<int,int>freq;
+       for(int i=0;i<n;i++){
+        freq[changed[i]]++;
+       }
+       for(int i=0;i<n;i++){
+        if(freq[changed[i]]==0) continue;
+        if(freq[2*changed[i]]==0) return {};
+        original.push_back(changed[i]);
+        freq[changed[i]]--;
+        freq[2*changed[i]]--;
+       } 
+       if(original.size()== n/2) return original;
+       else return {};
+
     }
 };
