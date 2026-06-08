8f149c3b81788b80276354ce55ce271ab2dca792
HarshArora25 <harsharora.2207@gmail.com>
2026-06-08 15:52:07 +0530

    Time: 1212 ms (5.11%) | Memory: 50.3 MB (81.57%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
@@ -1,22 +1,24 @@
 class Solution {
     private:
-    void sums(vector<int>&nums,vector<int>&y){
+    int even(vector<int>&nums){
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            //  sum=0;
-            if(nums[i]%2==0)
-          sum+=nums[i];
+           if(nums[i]%2==0)
+           sum=sum+nums[i];
         }
-        y.push_back(sum);
+        return sum;
     }
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int>y;
-        for(int i=0;i<queries.size();i++){
-            int sum=0;
-           nums[queries[i][1]]=nums[queries[i][1]]+queries[i][0];
-           sums(nums,y);
-        }
-        return y;
+     int n=queries.size();
+     vector<int>vec;
+     for(int i=0;i<n;i++){
+        int add=queries[i][0];
+        int idx=queries[i][1];
+        nums[idx]=nums[idx]+add;
+        int x=even(nums);
+        vec.push_back(x);
+     }   
+     return vec;
     }
 };
