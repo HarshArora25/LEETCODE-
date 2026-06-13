5d750c4d3b5694ea12ca2bbd639173c96c8f3d33
HarshArora25 <harsharora.2207@gmail.com>
2026-06-13 11:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (28.85%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,37 +1,42 @@
 class Solution {
-    int step;
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-      queue<pair<pair<int,int>,int>>qu;
-      vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
-      int nr[]={-1,0,1,0};
-      int nc[]={0,1,0,-1};
-      int fresh=0;
-      for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[0].size();j++){
-           if(grid[i][j]==2){
-            qu.push({{i,j},0});
-            vis[i][j]=1;
-           }
-           if(grid[i][j]==1)
-           fresh++;
+     int n=grid.size();
+     int m=grid[0].size();
+     queue<pair<int,pair<int,int>>>qu;
+     
+     vector<vector<int>>visited(n,vector<int>(m,0));
+     int fresh=0;
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(grid[i][j]==2){
+            qu.push({0,{i,j}});
+            visited[i][j]=1;
+          }
+          else if(grid[i][j]==1)
+          fresh++;
         }
-      }  
-      while(!qu.empty()){
-       int x=qu.front().first.first;
-       int y=qu.front().first.second;
-      step=qu.front().second;
-       qu.pop();
-       for(int i=0;i<4;i++){
-        int newr=nr[i]+x;
-        int newc=nc[i]+y;
-        if(newr>=0 && newr<grid.size() && newc>=0 && newc<grid[0].size() && !vis[newr][newc] && grid[newr][newc]==1){
-            qu.push({{newr,newc},step+1});
-            vis[newr][newc]=1;
-            fresh--;
+     } 
+     int nr[]={-1,0,1,0};
+     int nc[]={0,1,0,-1};  
+     int maxi=0;
+     while(!qu.empty()){
+        int step=qu.front().first;
+        int x=qu.front().second.first;
+        int y=qu.front().second.second;
+        maxi=max(maxi,step);
+        qu.pop();
+        for(int i=0;i<4;i++){
+            int newr=nr[i]+x;
+            int newc=nc[i]+y;
+            if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]==1 && visited[newr][newc]==0){
+               qu.push({step+1,{newr,newc}});
+               visited[newr][newc]=1;
+               fresh--;
+            }
         }
-       }
-      }
-      return (fresh>0)?-1:step;
+     }
+     if(fresh>0) return -1;
+     return maxi;
     }
 };
