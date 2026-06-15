a8b223b245418c9fa11b552dea32a3b94944a48a
HarshArora25 <harsharora.2207@gmail.com>
2026-06-15 16:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (92.72%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,26 +1,25 @@
 class Solution {
     private:
-    int vowels(string x,vector<char>&z){
-        int count=0;
-        for(int i=0;i<x.size();i++){
-           char c=tolower(x[i]);
-           for(int j=0;j<z.size();j++){
-            if(c==z[j])
-            count++;
-           }
-        }
-        return count;
+    bool isvowel(char f){
+        if(f=='a' || f=='e' || f=='i'  || f=='o' || f=='u' || f=='A' ||f=='E' ||f=='I' ||f=='O' ||f=='U')
+        return true;
+        else 
+        return false;
     }
 public:
     bool halvesAreAlike(string s) {
-        vector<char>z={'a','e','i','o','u'};
-        string x="",y="";
-        for(int i=0;i<s.size()/2;i++){
-        x=x+s[i];
-        y=y+s[s.size()/2+i];
-    }
-       int a=vowels(x,z);
-       int b=vowels(y,z);
-       return a==b;
+       int n=s.size();
+       if(n%2 !=0) return false;
+       int frsthalf=0,scndhlf=0;
+       int m=n/2;
+       for(int i=0;i<m;i++){
+        if(isvowel(s[i]))
+        frsthalf++;
+       } 
+       for(int i=m;i<n;i++){
+        if(isvowel(s[i]))
+        scndhlf++;
+       }
+       return frsthalf==scndhlf;
     }
 };
