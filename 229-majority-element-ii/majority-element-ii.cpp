b2ebf4c51c83c6443f894e3ebeafa60992aaa3b4
HarshArora25 <harsharora.2207@gmail.com>
2026-07-01 12:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (63.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        vector<int>elem;
-        int count=1;
-        // if(n<3) return nums;
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<=n;i++){
-          if(i<n && nums[i-1]==nums[i]){
-            count++;
-          }
-          else{
-
-                if(count > n/3)
-                    elem.push_back(nums[i-1]);
-
-                count = 1;
-          }
+        int cd=0;
+        int cd2=0;
+        int ct=0;
+        int ct2=0;
+        for(int num:nums){
+            if(num==cd)
+            ct++;
+            else if(num==cd2)
+            ct2++;
+            else if(ct==0){
+                cd=num;
+                ct++;
+            }
+            else if(ct2==0){
+                cd2=num;
+                ct2++;
+            }
+            else{
+                ct--;
+                ct2--;
+            }
         }
-        return elem;
+         ct=0,ct2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==cd)
+            ct++;
+            else if(nums[i]==cd2)
+            ct2++;
+        }
+        if(ct>n/3 && ct2>n/3) return {cd,cd2};
+        else if(ct>n/3) return {cd};
+        else if(ct2>n/3) return {cd2};
+        else return{};
     }
 };
