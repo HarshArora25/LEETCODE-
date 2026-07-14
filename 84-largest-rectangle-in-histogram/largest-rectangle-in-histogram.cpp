d9cf4201eeed6a4b8e30c00d83694ac83cb2f84b
HarshArora25 <harsharora.2207@gmail.com>
2026-07-14 11:14:38 +0530

    Time: 39 ms (12.26%) | Memory: 93.6 MB (5.01%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,36 +1,50 @@
 class Solution {
-public:
-    void nsel(int n,vector<int>&nse,vector<int>height){
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && height[st.top()]>=height[i]){
-                st.pop();
-            }
-           nse[i]= st.empty()?n:st.top();
-            st.push(i);
-        }
-    }
+private:
+void nextsmallest(vector<int>&nsel,vector<int>heights,int n){
+    stack<int>st;
+for(int i=n-1;i>=0;i--){
+ while(!st.empty() && heights[st.top()]>=heights[i])
+ st.pop();
+ if(!st.empty()){
+ nsel[i]=st.top();
+ st.push(i);
+ }
+ else{
+    nsel[i]=n;
+    st.push(i);
+ }
+}
+}
 
-     void psel(int n,vector<int>&pse,vector<int>height){
-                stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && height[st.top()]>height[i]){
-                st.pop();
-            }
-            pse[i]=st.empty()?-1:st.top();
-            st.push(i);
-        }
-    }
+void prevsmallest(vector<int>&psel,vector<int>heights,int n){
+stack<int>st;
+for(int i=0;i<n;i++){
+ while(!st.empty() && heights[st.top()]>heights[i])
+ st.pop();
+ if(!st.empty()){
+ psel[i]=st.top();
+ st.push(i);
+ }
+ else{
+    psel[i]=-1;
+    st.push(i);
+ }
+}
+}
+
+public:
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int>nse(n),pse(n);
-        nsel(n,nse,heights);
-        psel(n,pse,heights);
-        int maxi=0;
-        for(int i=0;i<n;i++){
-         int width=nse[i]-pse[i]-1;
-         maxi=max(maxi,width*heights[i]);
-        }
-        return maxi;
+     vector<int>psel(n,-1);
+     vector<int>nsel(n,n);
+     nextsmallest(nsel,heights,n);
+     prevsmallest(psel,heights,n);   
+     int maxi=0;
+     for(int i=0;i<n;i++){
+     int width = nsel[i] - psel[i] - 1;
+     int area = width * heights[i];
+        maxi=max(maxi,area);
+     }
+     return maxi;
     }
 };
