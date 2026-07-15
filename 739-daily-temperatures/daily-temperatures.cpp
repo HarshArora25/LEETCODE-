012aea8aacceb332d64dfaa295b2214d093e579e
HarshArora25 <harsharora.2207@gmail.com>
2026-07-15 14:00:11 +0530

    Time: 20 ms (63.63%) | Memory: 102.9 MB (63.51%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,18 +1,25 @@
 class Solution {
+private:
+    void nextgreater(int n,vector<int>& temperatures,vector<int>& nge){
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && temperatures[st.top()]<=temperatures[i])
+            st.pop();
+            if(!st.empty()) nge[i]=st.top();
+            st.push(i);
+        }
+    }
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-       int n=temperatures.size();
-       vector<int>arr(n,0);
-       stack<int>st;
-       for(int i=n-1;i>=0;i--){
-        while (!st.empty() && temperatures[i] >= temperatures[st.top()]) 
-          st.pop();
-        if(st.empty()) arr[i]=0;
-        else{
-            arr[i]=st.top()-i;
+        int n=temperatures.size();
+        vector<int>nge(n,0);
+        nextgreater(n,temperatures,nge);
+        for(int i=0;i<n;i++){
+            if(nge[i]==0) continue;
+            else{
+                nge[i]=nge[i]-i;
+            }
         }
-        st.push(i);
-       } 
-       return arr;
+        return nge;
     }
 };
