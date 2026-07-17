a380e2823f6b46cc3cb3af226b145f6a0a6bdb59
HarshArora25 <harsharora.2207@gmail.com>
2026-07-17 16:57:12 +0530

    Time: 63 ms (5.07%) | Memory: 45.9 MB (5.00%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int minSwaps(string s) {
-     stack<char>st;
-     for(char& c:s){
-        if(c=='[')
-        st.push(c);
-        else if(!st.empty())
-        st.pop(); 
-     }
-     // size of stack is no of open bracket
-     return (st.size()+1)/2;
+        stack<int>st;
+        for(char& ch:s){
+           if(ch=='[')
+           st.push(ch);
+           else if(!st.empty())
+           st.pop();
+        }
+        return (st.size()+1)/2;
     }
 };
