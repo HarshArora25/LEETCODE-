945e53ad60f1a6e223befe1995f76415e649749f
HarshArora25 <harsharora.2207@gmail.com>
2026-07-17 15:51:40 +0530

    Time: 28 ms (46.00%) | Memory: 26.5 MB (33.76%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-    // aproach 1 ba remove krra hu bss
-    // stack<char>st;
-    // int count=0;
-    // for(int i=0;i<s.size();i++){
-    //  if(!st.empty() && s[i]=='a' && st.top()=='b'){
-    //  st.pop();
-    //  count++;
-    //  }
-    //  else{
-    //     st.push(s[i]);
-    //  }
-    // }
-    // return count;
-
-    // approach 2
-    int counta=0;
-    int countb=0;
-    int n=s.size();
-    int mini=INT_MAX;
-    vector<int>b(n,0);
-    vector<int>a(n,0);
-    for(int i=0;i<s.size();i++){
-       b[i]=countb;
-      if(s[i]=='b') countb++;
-    }
-    for(int i=n-1;i>=0;i--){
-        a[i]=counta;
-      if(s[i]=='a') counta++;
-    }
-    // int count;
-    for(int i=0;i<n;i++){
-        mini=min(mini,a[i]+b[i]);
-    }
-    return mini;
+     int count=0;
+     int n=s.size();
+     stack<char>st;
+     for(int i=0;i<n;i++){
+    //   if(st.empty()) st.push(s[i]);
+      if(!st.empty() && st.top()=='b' && s[i]=='a'){
+      st.pop();
+      count++;
+      continue;
+      }
+      st.push(s[i]);
+     }   
+    return count;
     }
 };
