31a5ccfab825f5c289fc1f0153c1173e53302dd9
HarshArora25 <harsharora.2207@gmail.com>
2026-07-18 12:40:07 +0530

    Time: 18 ms (71.69%) | Memory: 136.5 MB (5.34%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-      vector<int>result;
-      for(long long num: nums){
-         while(!result.empty()){
-            long long prev=result.back();
-            long long curr=num;
-            long long GCD=gcd(prev,curr);
-            if(GCD==1) break;
-            result.pop_back();
-            num = (prev / GCD) * num; 
-         }
-         result.push_back(num);
-      }  
-      return result;
+        vector<long long> st;
+
+        for (long long current : nums) {
+            while (!st.empty()) {
+                long long common = gcd(st.back(), current);
+
+                if (common == 1) {
+                    break;
+                }
+
+                current = (st.back() / common) * current;
+                st.pop_back();
+            }
+
+            st.push_back(current);
+        }
+
+        vector<int> result;
+
+        for (long long value : st) {
+            result.push_back(static_cast<int>(value));
+        }
+
+        return result;
     }
 };
