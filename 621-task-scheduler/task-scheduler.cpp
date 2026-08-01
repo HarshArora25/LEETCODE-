87a4979907324b994621aa057f6317d85d9b14ca
HarshArora25 <harsharora.2207@gmail.com>
2026-08-01 11:03:44 +0530

    Time: 33 ms (40.45%) | Memory: 47.8 MB (20.63%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-       vector<int>charr(26);
-       for(int i=0;i<tasks.size();i++){
-        charr[tasks[i]-'A']++;
-       } 
-       priority_queue<int>pq;
-       for(int i=0;i<26;i++){
-        if(charr[i]>0)
-        pq.push(charr[i]);
-       }
-       int count=0;
-       while(!pq.empty()){
+      int sz=tasks.size();
+      vector<int>mpp(26,0);
+      int time=0;
+      for(int i=0;i<sz;i++){
+       mpp[tasks[i]-'A']++;
+      }
+      priority_queue<int>pq;
+      for(int i=0;i<26;i++){
+        if(mpp[i]>0)
+        pq.push(mpp[i]);
+      }
+      while(!pq.empty()){
         vector<int>temp;
-        for(int i=1;i<=n+1;i++){
-            if(!pq.empty()){
-            int freq=pq.top();
-            freq--;
-            temp.push_back(freq);
-            pq.pop();
-        }
-        }
-        for(auto& it:temp){
-            if(it>0)
-            pq.push(it);
-        }
-        if(pq.empty())
-        count+=temp.size();
-        else
-        count+=(n+1);
+      for(int i=1;i<=n+1;i++){
+       if(!pq.empty()){
+         int elem=pq.top();
+         elem--;
+         temp.push_back(elem);
+         pq.pop();
        }
-       return count;
+      } 
+      for(auto& fr:temp){
+        if(fr>0)
+        pq.push(fr);
+      } 
+      if(pq.empty())
+      time+=temp.size();
+      else
+      time+=n+1;
+      }
+      return time;
     }
 };
