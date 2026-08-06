3a66c02969a7709cd473aa2e89b7550946cd85cc
HarshArora25 <harsharora.2207@gmail.com>
2026-08-06 20:32:33 +0530

    Time: 4 ms (5.52%) | Memory: 18.4 MB (30.97%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,25 +1,28 @@
 class Solution {
-private:
-  void dfs(vector<vector<int>>& image,int sr,int sc,int oldcolor,int newcolor,int n,int m){
-    image[sr][sc]=newcolor;
-    int nr[]={-1,0,1,0};
-    int nc[]={0,-1,0,1};
-    for(int i=0;i<4;i++){
-        int nowr=sr + nr[i];
-        int nowc=sc + nc[i];
-        if(nowr>=0 &&nowr<n && nowc>=0 && nowc<m && image[nowr][nowc]==oldcolor){
-            image[nowr][nowc]=newcolor;
-        dfs(image,nowr,nowc,oldcolor,newcolor,n,m);
-        }
-    }
-  }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc]==color) return image;
       int n=image.size();
       int m=image[0].size();
-      int oldcolor=image[sr][sc];
-      if(oldcolor==color) return image;
-      dfs(image,sr,sc,oldcolor,color,n,m);
-      return image;
+      int nr[]={-1,0,1,0};
+      int nc[]={0,1,0,-1};
+      int old=image[sr][sc];
+      queue<pair<int,int>>qu;
+      qu.push({sr,sc});
+      image[sr][sc]=color;
+      while(!qu.empty()){
+         int x=qu.front().first;
+         int y=qu.front().second;
+        qu.pop();
+      for(int i=0;i<4;i++){
+        int newr=nr[i]+x;
+        int newc=nc[i]+y;
+        if(newr>=0 && newr<n && newc>=0 && newc<m && image[newr][newc]==old){
+        image[newr][newc]=color;
+        qu.push({newr,newc});
+        }
+      }
+    }
+    return image;
     }
 };
