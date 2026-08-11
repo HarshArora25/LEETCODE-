2d947255f14a08cba463c2c558e0bb9b50766c21
HarshArora25 <harsharora.2207@gmail.com>
2026-08-11 14:28:28 +0530

    Time: 22 ms (40.27%) | Memory: 26.3 MB (36.24%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]==1) return -1;
-        queue<pair<pair<int,int>,int>>qu;
-        qu.push({{0,0,},0}) ;  // 1st pos=x 2nd ->y 3rd dist;
-         vector<vector<int>>distance(grid.size(),vector<int>(grid[0].size(),1e9));
-         distance[0][0]=0;
-        while(!qu.empty()){
-        auto& it=qu.front();
-      int x = it.first.first;
-     int y = it.first.second;
-       int dist = it.second;
+     int n=grid.size();
+     int m=grid[0].size();
+      if(grid[0][0] == 1 || grid[grid.size()-1][grid.size()-1] == 1)
+             return -1;       
+     queue<pair<int,pair<int,int>>>qu;
+     vector<int>nr={-1,-1,-1,0,0,1,1,1};
+     vector<int>nc={-1,0,1,-1,1,-1,0,1};
+     qu.push({0,{0,0}}) ;
+     vector<vector<int>>dist(n,vector<int>(m,1e9));
+     dist[0][0]=0;
+     while(!qu.empty()){
+        int dis=qu.front().first;
+        int x=qu.front().second.first;
+        int y=qu.front().second.second;
         qu.pop();
-        int delr[]={-1,-1,1,1,0,-1,0,1};
-        int delc[]={-1,1,-1,1,1,0,-1,0};
-        for(int i=0;i<8;i++){
-            int nr=delr[i]+x;
-            int nc=delc[i]+y;
-            if(nr>=0 && nr<grid.size() && nc>=0 && nc<grid[0].size() && grid[nr][nc]==0 && distance[nr][nc]>1+dist){
-                distance[nr][nc]=1+dist;
-                qu.push({{nr,nc,},dist+1});
-            }
+    for(int i=0;i<8;i++){
+        int newr=x+nr[i];
+        int newc=y+nc[i];
+        if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]==0){
+           if(dist[newr][newc]>dis+1){
+           dist[newr][newc]=dis+1;  
+           qu.push({dist[newr][newc],{newr,newc}});
+           }
         }
-        }
-        if(distance[grid.size()-1][grid[0].size()-1]==1e9) return -1;
-        else
-        return distance[grid.size()-1][grid[0].size()-1]+1;
+    }
+     }
+        if(dist[n-1][m-1]==1e9) return -1; 
+        return dist[n-1][m-1]+1;
     }
 };
