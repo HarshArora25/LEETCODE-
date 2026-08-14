a97c99df3adf8a9f4f08d8fbd9eee0800988a969
HarshArora25 <harsharora.2207@gmail.com>
2026-08-14 12:15:45 +0530

    Time: 10 ms (87.11%) | Memory: 16.5 MB (96.77%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-    //    vector<int>dist(edges.size(),1e9); 
-    // int n=edges.size();
-    // int m=edges[0].size();
-    vector<vector<int>>dist(n,vector<int>(n,1e9));
-    for(int i=0;i<n;i++)
-         dist[i][i]=0;
-            for(auto& it:edges){
-                int u=it[0];
-                int v=it[1];
-                int wt=it[2];
-              dist[u][v]=wt;
-              dist[v][u]=wt;
-            }
-
-           for(int k=0;k<n;k++){
-             for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                 if(dist[i][k]==1e9 || dist[k][j]==1e9)
-                 continue;
-                 else{
-                    dist[i][j]=min(dist[i][k]+dist[k][j],dist[i][j]);
-                 }
+        vector<vector<int>>mat(n,vector<int>(n,1e9));
+        for(auto& it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            mat[u][v]=wt;
+            mat[v][u]=wt;
+        }
+        for(int i=0;i<n;i++) mat[i][i]=0;
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(mat[i][k] !=1e9 && mat[k][j] !=1e9)
+                  mat[i][j]=min(mat[i][k]+mat[k][j],mat[i][j]);
                 }
             }
-           }
-           int mincount=INT_MAX;
-           int city=-1;
-           for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(dist[i][j]!=1e9 && dist[i][j]<=distanceThreshold)
-                count++;
-            }
-            if(count<=mincount){
-                mincount=count;
-                city=i;
-            }
-           }
-           return city;
+        }
+      int state=-1,mini=1e9;
+      for(int i=0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(mat[i][j]<=distanceThreshold)
+            count++;
+        }
+        if(count<=mini){
+            mini=count;
+            state=i;
+        }
+      }
+      return state;
     }
 };
