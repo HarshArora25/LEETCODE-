e85d39b904dd339af69889654e77e5bf7a6ef86d
HarshArora25 <harsharora.2207@gmail.com>
2026-08-15 15:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (26.27%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
-        vector<int>visited(n,0);
-        // vector<vector<int>>adjl(n);
-        queue<int>qu;
-        qu.push(0);
-        // for(int i=0;i<n;i++){
-        //     adjl[i].push_back(rooms[i].begin(),rooms[i].end());
-        // }
-        visited[0]=1;
-        while(!qu.empty()){
-         int elem=qu.front();
-         qu.pop();
-         for(auto& it: rooms[elem]){
-            if(!visited[it]){
-                visited[it]=1;
-                qu.push(it);
-            }
-         }
+     int n=rooms.size();
+    //  vector<int>adjl[n];
+
+    //  for(int i=0;i<n;i++){
+    //  for(int j=0;j<rooms[i].size();j++){
+    //    adjl[i].push(rooms[j]);
+    //  }   
+    //  }
+    queue<int>qu;
+     vector<int>visited(n,0);
+     qu.push(0);
+     visited[0]=1;
+     while(!qu.empty()){
+        int fr=qu.front();
+        qu.pop();
+       for(auto& it: rooms[fr]){
+        if(!visited[it]){
+            visited[it]=1;
+            qu.push(it);
         }
-        for(int i=0;i<n;i++){
-            if(visited[i]==0) return false;
-        }
-        return true;
+       }
+     }
+     for(int i=0;i<n;i++){
+        if(visited[i]==false)
+        return false;
+     }
+     return true;
     }
 };
