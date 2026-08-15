7c1c7d063aea644f254e6e7eff8abac065e09875
HarshArora25 <harsharora.2207@gmail.com>
2026-08-15 12:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (16.01%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        unordered_set<string>visited;
-         unordered_set<string>bankset(bank.begin(),bank.end());
-        queue<string>q;
-        q.push(startGene);
-        visited.insert(startGene);
-        int level=0;
-        while(!q.empty()){
-            int size=q.size();
-        while(size--){
-            string x=q.front();
-            q.pop();
-        if(x==endGene) return level;
-        for(char ch : "ACGT"){
-            for(int i=0;i<x.size();i++){
-                string res=x;
-                res[i]=ch;
-            if(visited.find(res)==visited.end() && bankset.find(res)!=bankset.end()){
-                visited.insert(res);
-                q.push(res);
-            }
+        int n=startGene.size();
+    unordered_set<string> stt(bank.begin(), bank.end());
+    queue<pair<string,int>>qu;
+    qu.push({startGene,0});
+    vector<char>letters({'A','C','G','T'});
+    while(!qu.empty()){
+        string fnd=qu.front().first;
+        int lvl=qu.front().second;
+        qu.pop();
+        if(fnd==endGene) return lvl;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<n;j++){
+          string org=fnd;
+          org[j]=letters[i];
+          if(stt.find(org) !=stt.end()){
+            qu.push({org,lvl+1});
+            stt.erase(org);
+            continue;
+          }          
+        }
 
-            }
-        }
-        }
-        level++;
-        }
-        return -1;
+    }
+
+    }
+    return -1;
     }
 };
