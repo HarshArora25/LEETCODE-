32260352edcc837bd5ff56976eef20fbf63f0dfd
HarshArora25 <harsharora.2207@gmail.com>
2026-08-16 12:55:32 +0530

    Time: 23 ms (7.02%) | Memory: 22.4 MB (6.14%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,21 +1,25 @@
 class Solution {
 private:
-    void dfs(int i,vector<vector<int>>&graph,vector<vector<int>>&x,vector<int>y){
-         y.push_back(i);
-        if(i==graph.size()-1){
-            x.push_back(y);
+ void dfs(vector<vector<int>>& graph,vector<int>visited,int n, vector<vector<int>>&finres,vector<int>res,int i){
+    visited[i]=1;
+    res.push_back(i);
+    if(i==n-1){
+         finres.push_back(res);
          return ;
-        } 
-        for(auto&it:graph[i]){
-         dfs(it,graph,x,y);
-        }
-        y.pop_back();
     }
+    for(auto& it:graph[i]){
+     dfs(graph,visited,n,finres,res,it);
+    }
+     visited[i]=0;
+     res.pop_back();
+ }
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-       vector<vector<int>>x;
-       vector<int>y;
-        dfs(0,graph,x,y); 
-      return x;
+     int n=graph.size();
+     vector<int>visited(n,0);
+     vector<vector<int>>finres;
+     vector<int>res;
+        dfs(graph,visited,n,finres,res,0);
+     return finres; 
     }
 };
