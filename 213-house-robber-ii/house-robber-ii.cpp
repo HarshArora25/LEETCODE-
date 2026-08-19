4e310094c34a226feadeeeabb9fe710f4759295b
HarshArora25 <harsharora.2207@gmail.com>
2026-08-19 11:39:57 +0530

    Time: 2 ms (8.82%) | Memory: 10.9 MB (18.93%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
     private:
-    int helper(vector<int>&nums,int i,vector<int>&dp){
+    int helper(vector<int>&nums,int &sum,int i,vector<int>&dp){
       if(i==0) return nums[0];
       if(i==1) return max(nums[0],nums[i]);
         if (dp[i] != -1) return dp[i];
-      dp[i]=max((nums[i]+helper(nums,i-2,dp)),helper(nums,i-1,dp));
+      dp[i]=max((nums[i]+helper(nums,sum,i-2,dp)),helper(nums,sum,i-1,dp));
       return dp[i];
     }
 public:
     int rob(vector<int>& nums) {
       int sum=0;
       if(nums.size()==1) return nums[0];
-      int i=nums.size()-1;
-      vector<int>dp1(i+1,-1);
-        vector<int>dp2(i+1,-1);
+      int n=nums.size();
+      int i=nums.size()-2;
+      vector<int>dp1(n+1,-1);
+      vector<int>dp2(n+1,-1);
       vector<int>a(nums.begin()+1,nums.end());
       vector<int>b(nums.begin(),nums.end()-1);
-     int x=helper(a,i-1,dp1);  
-     int y=helper(b,i-1,dp2);
-      return max(x,y); 
+     return max(helper(a,sum,i,dp1), helper(b,sum,i,dp2));  
     }
 };
