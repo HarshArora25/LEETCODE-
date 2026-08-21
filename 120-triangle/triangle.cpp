14ab25397faaef0a7d5f11570efa7752b26c8e6e
HarshArora25 <harsharora.2207@gmail.com>
2026-08-21 11:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (26.04%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,23 +1,23 @@
 class Solution {
 // private:
-//     int helpme(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-//     if (i==n-1) return dp[i][j]=triangle[i][j];
-//     if(dp[i][j] !=-1) return dp[i][j];
-//     int left=helpme(i+1,j,n,triangle,dp);
-//     int right=helpme(i+1,j+1,n,triangle,dp);
-//     return dp[i][j]=triangle[i][j]+min(left,right);
+//     int help(vector<vector<int>>& triangle,int row,int col,vector<vector<int>>& dp){
+//         if(row==triangle.size()-1) return triangle[row][col];
+//         if(dp[row][col] !=-1) return dp[row][col];
+//         int down=help(triangle,row+1,col,dp);
+//         int diag=help(triangle,row+1,col+1,dp);
+//         return dp[row][col]=triangle[row][col]+min(down,diag);
 //     }
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-     int n=triangle.size();
-     vector<vector<int>>dp(n,vector<int>(n,-1));
-     for(int i=n-1;i>=0;i--)
-     dp[n-1][i]=triangle[n-1][i];
-     for(int i=n-2;i>=0;i--){
-        for(int j=0;j<=i;j++){
-         dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+    int  minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size()-1;
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                if(i==n) dp[i][j]=triangle[i][j];
+                else
+                dp[i][j]=triangle[i][j]+min(dp[i+1][j+1],dp[i+1][j]);
+            }
         }
-     }
-       return dp[0][0];
+        return dp[0][0];
     }
 };
