77eadf77595ce582430b6ffc7cf88578f834a164
HarshArora25 <harsharora.2207@gmail.com>
2026-08-22 14:39:41 +0530

    Time: 207 ms (9.71%) | Memory: 128.3 MB (12.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
-private:
-  bool btabta(vector<int>& nums,int idx,int find, vector<vector<int>>&dp){
-    if(find==0) return true;
-    if(idx<0) return false;
-        if(dp[idx][find] !=-1) return dp[idx][find];
-    bool nottake=btabta(nums,idx-1,find,dp);
+private: 
+  bool help(int target, vector<vector<int>>&dp,int idx,vector<int>& nums){
+    if(target==0) return true;
+   if (idx == 0) return nums[0] == target;
+    if(idx<0 || target<0 ) return false;
+    if(dp[target][idx] !=-1) return dp[target][idx];
+    bool nottake=help(target,dp,idx-1,nums);
     bool take=false;
-    if(find>=nums[idx]){
-        take=btabta(nums,idx-1,find-nums[idx],dp);
-    }
-    return dp[idx][find]=take || nottake;
+    if(nums[idx]<=target)
+    take=help(target-nums[idx],dp,idx-1,nums);
+    return dp[target][idx]=take || nottake;
   }
 public:
     bool canPartition(vector<int>& nums) {
-      int idx=nums.size()-1;
-      int total=0;
-      for(int i=0;i<=idx;i++){
-        total+=nums[i];
-      }
-      if(total%2 !=0) return false;
-      vector<vector<int>>dp(idx+1,vector<int>(total/2+1,-1));
-    return  btabta(nums,idx,total/2,dp);  
+       int sum=0;
+       int n=nums.size();
+       for(auto it:nums)
+       sum+=it;
+        if(sum % 2 !=0) return false;
+        int target=sum/2;
+        vector<vector<int>>dp(target+1,vector<int>(n+1,-1));
+       return  help(target,dp,n-1,nums);
     }
 };
