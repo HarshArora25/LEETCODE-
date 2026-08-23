7ebfcc59ae5f74f1b719ec33653426b81f72ca85
HarshArora25 <harsharora.2207@gmail.com>
2026-08-24 00:16:38 +0530

    Time: 25 ms (69.66%) | Memory: 27.5 MB (55.63%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,30 +1,29 @@
 class Solution {
- public:
-    // int helper(string &text1,string &text2,int idx1,int idx2, vector<vector<int>>&dp){
-    // if(idx1<0 || idx2<0) return 0;
-    // if(dp[idx1][idx2] !=-1) return dp[idx1][idx2];
-    // if(text1[idx1]==text2[idx2])
-    // return dp[idx1][idx2]=1+helper(text1,text2,idx1-1,idx2-1,dp);
-    //  else{
-    // return dp[idx1][idx2]=max(helper(text1,text2,idx1-1,idx2,dp),helper(text1,text2,idx1,idx2-1,dp));
-    //  }
-    //  return 0;
-    // }
+// private:
+//   int helper(string text1, string text2,int n,int m,int i,int j,vector<vector<int>>&dp){
+//     if(i>=n || j>=m) return 0;
+//     if(dp[i][j] !=-1) return dp[i][j];
+//     // if(j==m-1 && text1[i]==text2[j]) return 1;
+//     if(text1[i]==text2[j]){
+//     return  dp[i][j]= 1+helper(text1,text2,n,m,i+1,j+1,dp);
+//     }
+//      int skip1=helper(text1,text2,n,m,i+1,j,dp);
+//      int skip2=helper(text1,text2,n,m,i,j+1,dp);
+//      return dp[i][j]= max(skip1,skip2);
+//   }
+public:
     int longestCommonSubsequence(string text1, string text2) {
-        int idx1=text1.size();
-        int idx2=text2.size();
-         vector<vector<int>>dp(idx1+1,vector<int>(idx2+1,-1));
-        for(int i=0;i<=idx1;i++) dp[i][0]=0;
-        for(int j=0;j<=idx2;j++) dp[0][j]=0;
-
-        for(int i=1;i<=idx1;i++){
-            for(int j=1;j<=idx2;j++){
-                if(text1[i-1]==text2[j-1])
-                dp[i][j]=1+dp[i-1][j-1];
-                else
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+      int n=text1.size();
+      int m=text2.size();
+      vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+           if(text1[i-1]==text2[j-1])
+           dp[i][j]=1+dp[i-1][j-1];
+           else
+           dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
-        return dp[idx1][idx2];
+      }
+       return dp[n][m];
     }
 };
