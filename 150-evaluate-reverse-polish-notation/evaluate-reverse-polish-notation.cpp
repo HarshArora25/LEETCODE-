e0b6f1c58ad3430f1f7e1f4ef88ac372019e6292
HarshArora25 <harsharora.2207@gmail.com>
2026-09-09 10:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (14.14%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    int evalRPN(vector<string>& s) {
-       stack<int>st;
-       int n=s.size();
+    int evalRPN(vector<string>& tokens) {
+        int n=tokens.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(tokens[i]=="+"){
+                int frst=st.top();
+                st.pop();
+                int scnd=st.top();
+                st.pop();
+                int thrd=frst+scnd;
+                st.push(thrd);
+            }
+            else if(tokens[i]=="-"){
+                int frst=st.top();
+                st.pop();
+                int scnd=st.top();
+                st.pop();
+                int thrd=scnd-frst;
+                st.push(thrd);
+            }
 
-       for(int i=0;i<n;i++){
-         if(s[i]!="+" && s[i]!="-" && s[i]!="*" && s[i]!="/")
-         st.push(stoi(s[i]));
-       else{
-        int top2=st.top();
-        st.pop();
-        int top1=st.top();
-        st.pop();
-        if(s[i]=="+")
-        st.push(top1 +top2 );
-         else if(s[i]=="-")
-         st.push(top1 - top2);
-         else if(s[i]=="*")
-         st.push(top1 * top2);
-         else
-         st.push(top1 / top2);
-       }
-       }
-       return st.top();
+            else if(tokens[i]=="*"){
+                int frst=st.top();
+                st.pop();
+                int scnd=st.top();
+                st.pop();
+                int thrd=frst*scnd;
+                st.push(thrd);
+            }
+
+            else if(tokens[i]=="/"){
+                int frst=st.top();
+                st.pop();
+                int scnd=st.top();
+                st.pop();
+                int thrd=scnd/frst;
+                st.push(thrd);
+            }
+            else
+            st.push(stoi(tokens[i]));
+        }
+        return st.top();
     }
 };
