3ae9a5072fded2de78e06643408918a65a90dc40
HarshArora25 <harsharora.2207@gmail.com>
2026-09-09 13:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.86%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-      int open=0,close=0;
-      for(int i=0;i<s.size();i++){
+     int n=s.size();
+     int i=0;
+     int open=0;
+     int close=0;
+     for(int i=0;i<n;i++){
         if(s[i]=='(')
         open++;
         else if(s[i]==')' && open>0)
         open--;
         else
         close++;
-      }  
-      return open+close;
+     }   
+     return open+close;
     }
 };
