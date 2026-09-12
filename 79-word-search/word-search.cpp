c30c00a5be83da42f594b69c4fbb92db4289162c
HarshArora25 <harsharora.2207@gmail.com>
2026-09-13 00:54:58 +0530

    Time: 777 ms (27.85%) | Memory: 11.7 MB (29.10%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,34 +1,32 @@
 class Solution {
 private:
-   vector<vector<int>>directions{{1,0},{0,-1},{-1,0},{0,1}};
-   bool helper(int n,int m,vector<vector<char>>& board,string word,int i,int j,int idx){
-         if(idx==word.size()) return true;
-         if(i < 0 || j < 0 || i >= n || j >= m) return false;
-         if(board[i][j] != word[idx]) return false;
-         char temp=board[i][j];
-         board[i][j]='$';
-         for(auto&dir : directions){
-            int new_i=i+dir[0];
-            int new_j=j+dir[1];
-
-            if(helper(n,m,board,word,new_i,new_j,idx+1))
-            return true;
-         }
-            board[i][j]=temp;
-            return false;
-   } 
+ bool solve(vector<vector<char>>& board, string word,int n,int m,int i,int j,int index){
+    if(i<0 || i>=n || j<0 || j>=m) return false;
+    if(board[i][j] != word[index]) return false;
+    if(word.size()-1==index) return true;
+    char ch=board[i][j];
+    board[i][j]='#';
+     bool found =
+            solve(board, word, n, m, i + 1, j, index + 1) ||
+            solve(board, word, n, m, i - 1, j, index + 1) ||
+            solve(board, word, n, m, i, j + 1, index + 1) ||
+            solve(board, word, n, m, i, j - 1, index + 1);
+    board[i][j]=ch;
+    return found;
+ }
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size();
-        int m=board[0].size();
-
-    for(int i=0;i<n;i++){
+      int n=board.size();
+      int m=board[0].size();
+      int index=0;
+      for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(board[i][j]==word[0] && helper(n,m,board,word,i,j,0)){
-                return true;
+            if(board[i][j]==word[0]){
+          if(solve(board,word,n,m,i,j,index))
+          return true;
             }
         }
-    }
-            return false;
+      } 
+          return false;
     }
 };
