8ce82b327129d95302a83ecea0a00db130b04769
HarshArora25 <harsharora.2207@gmail.com>
2026-09-15 06:50:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.88%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    double solve(double x, long n){
+        if(n==0) return 1;
+        if(n<0)
+       return solve(1/x,-n);
+       else if(n % 2 !=0)
+       return x*solve(x*x,(n-1)/2);
+        else
+      return  solve(x*x,n/2);
+
+    }
+public:
+    double myPow(double x, int n) {
+     return solve(x,(long)n);
+    }
+};
