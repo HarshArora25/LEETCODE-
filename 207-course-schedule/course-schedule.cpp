bab9680eb72d6ff8d5ebd304cfc58bbf4f8a80b4
HarshArora25 <harsharora.2207@gmail.com>
2026-09-16 17:02:34 +0530

    Time: 11 ms (21.67%) | Memory: 20.4 MB (11.16%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,33 @@
 class Solution {
+private:
+     bool dfs(int numCourses,vector<int>&visited,vector<int>&path,int i,vector<vector<int>>&adjl){
+        visited[i]=1;
+        path[i]=1;
+        for(auto it:adjl[i]){
+            if(!visited[it]){
+                if(dfs(numCourses,visited,path,it,adjl))
+                return true;
+            }
+            else if(path[it])
+            return true;
+        }
+        path[i]=0;
+        return false;
+     }
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& x) {
-      int n=x.size();
-      queue<int>qu;
-      vector<int>indegree(numCourses,0);
-      vector<vector<int>>adjl(numCourses);
-      for(auto it:x){
-        adjl[it[0]].push_back(it[1]);
-      }
-      for(auto it:x){
-        indegree[it[1]]++;
-      }
-      for(int i=0;i<numCourses;i++){
-        if(indegree[i]==0)
-           qu.push(i);
-      }
-      vector<int>ans;
-      while(!qu.empty()){
-       int fr=qu.front();
-       ans.push_back(fr);
-       qu.pop();
-       for(auto& it:adjl[fr]){
-        indegree[it]--;
-        if(indegree[it]==0)
-        qu.push(it);
-       }
-      }
-         return ans.size()==numCourses;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adjl(numCourses);
+        for(auto it:prerequisites){
+            adjl[it[0]].push_back(it[1]);
+        }
+    vector<int>visited(numCourses,0);
+    vector<int>path(numCourses,0);
+    for(int i=0;i<numCourses;i++){
+        if(!visited[i]){
+        if(dfs(numCourses,visited,path,i,adjl))
+        return false;
+        }
+    } 
+    return true;
     }
 };
